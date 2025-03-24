a0fd2b3302d1b273d480b8da90f9ca2014273930
ACken <kendallangulo2@gmail.com>
2025-03-23 21:39:30 -0600

    I modified the fields of the user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
+            $table->string('role');
+            $table->json('permissions')->nullable();
             $table->rememberToken();
-            $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->id();
+            $table->foreignId('user_id')->nullable()->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
@@ -47,3 +47,4 @@ return new class extends Migration
         Schema::dropIfExists('sessions');
     }
 };
+
