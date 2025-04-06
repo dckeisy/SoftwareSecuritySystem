a270337331fe955ef71d8fe81725be92a46e4b0c
dckeisy <keisyangulo15@gmail.com>
2025-04-06 11:06:25 -0600

    feat: Add Methods for create, update, delete and list products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+   public function index()
+   {
+    $products = Product::all();
+    return view("products.index", compact("products"));
+   }
+
+   public function create()
+   {
+    return view("products.create");
+   }
+   public function store(Request $request)
+   {
+    $request->validate([
+        'code' => 'required|unique:products,code',
+        'name' => 'required',
+        'description' => 'required',
+        'quantity' => 'required|integer',
+        'price' => 'required|numeric',
+    ]);
+    Product::create($request->all());
+    return redirect()->route('products.index')->with('success','Producto creado.');
+
+   }
+   public function edit(Product $product)
+   {
+    return view('products.edit', compact('product'));
+   }
+   public function update(Request $request, Product $product)
+   {
+    $request->validate([
+        'code' => 'required|unique:products,code,'.$product->id,
+        'name' => 'required',
+        'description' => 'required',
+        'quantity' => 'required|integer',
+        'price' => 'required|numeric',
+    ]);
+    $product->update($request->all());
+    return redirect()->route('products.index')->with('success','Producto actualizado.');
+   }
+   public function destroy(Product $product)
+   {
+    $product->delete();
+    return redirect()->route('products.index')->with('success','Producto eliminado.');
+
+   }
+
 }
