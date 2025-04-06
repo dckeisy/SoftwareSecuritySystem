a190b04832d3d600463dc96d7effef36dd7303b4
dckeisy <keisyangulo15@gmail.com>
2025-04-06 11:10:16 -0600

    feat add routes for products

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+// Gestión de Productos (para usuarios autenticados)
+Route::middleware('auth')->group(function(){
+    Route::resource('products', ProductController::class);
 });
 
 Route::get('/dashboard', function () {
