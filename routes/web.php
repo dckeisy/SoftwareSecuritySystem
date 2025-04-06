b02ee3fd773abb6097ec8c022d98069138956822
ACken <kendallangulo2@gmail.com>
2025-04-05 21:15:14 -0600

    Repair the routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,28 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Rutas para administrador
+Route::middleware(['auth', 'check.role:superadmin'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+    Route::post('register', [RegisteredUserController::class, 'store']);
+});
+
+// Rutas para usuario normal
+Route::middleware(['auth', 'check.role:usuario'])->group(function () {
+    Route::get('/userhome', function () {
+        return view('userhome');
+    })->name('userhome');
 });
 
 require __DIR__.'/auth.php';
