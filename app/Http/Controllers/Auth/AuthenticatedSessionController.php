7fdedfd1b1001fe8abb90972ce8f5675d38aa44c
ACken <kendallangulo2@gmail.com>
2025-04-11 17:59:01 -0600

    I fix the route with logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -37,6 +37,6 @@ class AuthenticatedSessionController extends Controller
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/');
+        return redirect('/home');
     }
 }
