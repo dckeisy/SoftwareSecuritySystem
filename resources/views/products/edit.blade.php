99f99a278f99d4a9077f76fee7fa0514564305f7
ACken <kendallangulo2@gmail.com>
2025-04-13 14:38:20 -0600

    Fixed user nav reference

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,4 +1,5 @@
-<x-app-layout>
+@include('profile/partials.navuser')
+<x-user-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Editar Producto') }}
@@ -46,4 +47,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-user-layout>
