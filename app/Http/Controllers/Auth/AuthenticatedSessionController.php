f76babdc1e6aafc35054f84590240ff3b2aa402d
ACken <kendallangulo2@gmail.com>
2025-04-13 13:17:33 -0600

    I fix the logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -55,6 +55,6 @@ class AuthenticatedSessionController extends Controller
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/home');
+        return redirect('/');
     }
 }
