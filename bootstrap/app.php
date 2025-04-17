07e289d6616d9ea5b384148f4b73f6d4d9ac1947
corralesK <kimcorralesv@gmail.com>
2025-04-17 13:46:07 -0600

    Config: Registration of middlewares in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckRole;
+use App\Http\Middleware\CheckEntityPermission;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'check.role' => CheckRole::class,
+            'check.entity.permission' => CheckEntityPermission::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
